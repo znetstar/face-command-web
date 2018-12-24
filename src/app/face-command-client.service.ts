e40006d7b00638f6a7f6980fe6350d42c4de1b76
Zachary Boyd <zachary@zacharyboyd.nyc>
2018-12-23 20:59:31 -0500

    Update multi-rpc

--- src/app/face-command-client.service.ts
+++ src/app/face-command-client.service.ts
@@ -19,13 +19,14 @@ export class FaceCommandClientService extends EventEmitter2 {
 
   constructor(private errors: AppErrorHandler, private snackbar: MatSnackBar) {
     super();
-    // this.resources = new AppResources(`${(location.protocol === 'https:') ? 'wss' : 'ws'}://${document.location.host}/rpc`);
-    this.resources = new AppResources(`${(location.protocol === 'https:') ? 'wss' : 'ws'}://127.0.0.1:7732/rpc`);
+    this.resources = new AppResources(`${(location.protocol === 'https:') ? 'wss' : 'ws'}://${document.location.host}/rpc`);
     this.commandService = new CommandService(this.resources);
     this.detectionService = new DetectionService(this.resources);
     this.faceManagementService = new FaceManagementService(this.resources);
     this.configService = new ConfigService(this.resources);
     this.logsService = new LogsService(this.resources);
+    this.resources.rpcClient.connect();
+
   }
 
   public static async faceImageAsDataUri(face: Face): Promise<string> {
